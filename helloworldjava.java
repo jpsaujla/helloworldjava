7ca1aa4ec9e5bd2e7a007c6dae06fafc9c6764f6
Jaspal S Aujla <94118499+jpsaujla@users.noreply.github.com>
2022-01-08 22:21:11 -0500

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps - webhook Polling");
+    System.out.println("Hello World from DevOps - Webhook Polling");
     }
 }
 
